8b7e5ea70f9d7401dc47fed54dad6176d9679f51
Franco Batastini <fbatastini@fi.uba.ar>
2020-11-05 10:59:46 -0300

    Debuggeado, y con destructor de lines.

--- parser.c
+++ parser.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "hash.h"
 
 
-char** read(char* file) {
+char** read(char* file, int* cant_lineas) {
     FILE *fp = fopen(file, "r");
     if(fp == NULL) {
         perror("Unable to open file!");
         exit(1);
 	}
-// Read lines using POSIX function getline
-// This code won't work on Windows
 	char *line = NULL;
 	size_t len = 0;
-	char** lines=malloc(sizeof(char*)*100);
-	int i =0;
- 	
+	char** lines = NULL;
 	while(getline(&line, &len, fp) != -1) 
 	{
-		//printf("%s", line);
-		lines[i]=malloc(sizeof(char)*len);
-		strcpy(lines[i], line);
-		i+=1;
+		lines = realloc(lines, sizeof(char*)*((*cant_lineas)+1));
+		lines[(*cant_lineas)]=malloc(sizeof(char)*len);
+		strcpy(lines[(*cant_lineas)], line);
+		(*cant_lineas)++;
 		
 	}
-    //printf("\n\nMax line size: %zd\n", len);
 	fclose(fp);
     free(line);
    	return lines;
 }
 
+void destruir_lineas(char** lines, int cantidad_lineas){
+	for (int i=0; i<cantidad_lineas; i++)
+		free(lines[i]);
+	free(lines);
+}
 
 int
 main(int argc, char * const argv[])
 {
-	
-	char** lines = read(argv[1]);
-	int i = sizeof(lines)-1;
-	for(int x=0;x<i;x++){
-    printf("%s", lines[x]);
+	int cant_lineas = 0;
+	char** lines = read("input.txt", &cant_lineas);
+	//char** lines = read(argv[1]);
+	for(int x=0; x < cant_lineas; x++){
+    	printf("%s", lines[x]);
     }
+	//string_hash_more();
+	destruir_lineas(lines, cant_lineas);
 	return 0;
 }
 
